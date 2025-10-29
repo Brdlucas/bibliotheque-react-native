3f62604e8deba58ac316c3cbc0b61bdd3424d175
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-10-29 15:49:12 +0100

    ajout function updateBooks pour mettre a jour un livre spécifique

--- services/BookServices.ts
+++ services/BookServices.ts
@@ -83,6 +83,32 @@ async function postNewBook(name: string, author: string, editor: string, year: n
 
 }
 
+async function updateBooks(id: number, name: string, author: string, editor: string, year: number){
+
+  try {
+    // création  d'un book a partir des informations fournies
+    // const response = await fetch(`https://api.books.tristan-renard.com/books/${id}`);
+    const response = await fetch(`http://localhost:3000/books/${id}`, {
+      method: "PUT",
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({name, author, editor, year})
+    });
+
+    console.log(response.status);
+
+    if(response.status === 200){
+      return response.status;
+    }else {
+      console.error('Erreur lors de la mise a jour du livre');
+      return response.status;
+    }
+
+  } catch (error) {
+    console.error(error);
+  }
+
+}
+
 
 async function getDeleteBook(id: number){
 
@@ -102,5 +128,5 @@ async function getDeleteBook(id: number){
   
   
 }
-export { getBooks, getDeleteBook, getDetailBook, postNewBook };
+export { getBooks, getDeleteBook, getDetailBook, postNewBook, updateBooks };
 
