8026d04144dfae5e98c016953427e5ea7b8b6db7
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-10-29 13:30:19 +0100

    création d'une fonction de récupération et suppression a partir de l'id donné

--- services/BookServices.ts
+++ services/BookServices.ts
@@ -3,9 +3,9 @@ import { Books } from "@/models/Books";
 async function getBooks(): Promise<Books[]> {
 
   // récupération des books a partir de l'api
-  const response = await fetch("https://api.books.tristan-renard.com/books");
+  // const response = await fetch("https://api.books.tristan-renard.com/books");
+  const response = await fetch("http://localhost:3000/books");
   const data = await response.json();
-  console.log("data", data);
 
 //   mise en forme de la variable books en récupérent les éléments pertinents
   const books = data.map(
@@ -48,5 +48,38 @@ async function getBooks(): Promise<Books[]> {
   return books;
 }
 
-export { getBooks };
+
+async function getDetailBook(id: number): Promise<Books>{
+
+  // récupération d'un seul book a partir de l'id
+  // const response = await fetch(`https://api.books.tristan-renard.com/books/${id}`);
+  const response = await fetch(`http://localhost:3000/books/${id}`);
+  const data = await response.json();
+
+  return data;
+}
+
+
+async function getDeleteBook(id: number){
+
+  try {
+    // récupération d'un seul book a partir de l'id
+    const response = await fetch(`http://localhost:3000/books/${id}`, {
+      method: "DELETE"
+    });
+  
+    const data = await response.json();
+  
+    if(response.status === 200){
+      console.log(data)
+      return data;
+    }
+    
+  } catch (error) {
+    console.error(error)
+  }
+  
+  
+}
+export { getBooks, getDeleteBook, getDetailBook };
 
