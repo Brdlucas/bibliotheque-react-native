23fc82aacb0b3951187b42c7de37017f258f2a6a
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-10-29 10:17:54 +0100

    Ajout du type Books pour modéliser les livres dans l'application

---
+++ models/Books.ts
@@ -0,0 +1,12 @@
+export type Books = {
+    id: number,
+    name: String,
+    author: String,
+    editor: String,
+    year: Number,
+    read: boolean,
+    favorite: boolean,
+    rating: number,
+    cover: String,
+    theme: String
+  };
