be91f516f9c961567ffde680f3f3cba5d08f240d
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-10-29 11:35:29 +0100

    création de la funtion getBooks asynchrone pour récupérer tout les livres de l'api

--- services/BookServices.ts
+++ services/BookServices.ts
@@ -1,14 +1,52 @@
+import { Books } from "@/models/Books";
 
+async function getBooks(): Promise<Books[]> {
 
-async function getBooks() {
+  // récupération des books a partir de l'api
+  const response = await fetch("https://api.books.tristan-renard.com/books");
+  const data = await response.json();
+  console.log("data", data);
 
-    const response = await fetch('http://localhost:3000/books');
-    const data = await response.json();
+//   mise en forme de la variable books en récupérent les éléments pertinents
+  const books = data.map(
+    ({
+      id,
+      name,
+      author,
+      editor,
+      year,
+      read,
+      favorite,
+      rating,
+      cover,
+      theme,
+    }: {
+      id: Number;
+      name: String;
+      author: String;
+      editor: String;
+      year: Number;
+      read: Boolean;
+      favorite: Boolean;
+      rating: Number;
+      cover: String;
+      theme: String;
+    }) => ({
+      id,
+      name,
+      author,
+      editor,
+      year,
+      read,
+      favorite,
+      rating,
+      cover,
+      theme,
+    })
+  );
 
-    return data
-};
+  return books;
+}
 
+export { getBooks };
 
-export default {
-    getBooks
-};
