5477fda7f428e748a98d77c0e0c4b91f9cfa7379
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-10-29 11:33:46 +0100

    Ajout de la fonctionnalité pour afficher les livres récupérés depuis l'API

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,36 @@
-import { Text, View } from "react-native";
+import BookCard from "@/components/BookCard";
+import { Books } from "@/models/Books";
+import { getBooks } from "@/services/BookServices";
+import { useEffect, useState } from "react";
+import { Pressable, ScrollView, StyleSheet, View } from "react-native";
 
 export default function Index() {
+  const [books, setBooks] = useState<Books[]>([]);
+
+  useEffect(() => {
+    getBooks().then(data => setBooks(data));
+  }, []);
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <ScrollView>
+        <View style={styles.container}>
+      { books.map((value, idx) => (
+        <Pressable onPress={() => console.log('id =', value.id)}>
+          <BookCard key={idx} {...value} />
+        </Pressable>
+          ))
+      }
+        </View>
+      </ScrollView>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 16,
+    padding: 12,
+  }
+})
