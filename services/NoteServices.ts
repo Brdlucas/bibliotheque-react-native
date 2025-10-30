7b7ade95cde33fc9a9a637a0b68ea79b6de762cb
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-10-30 18:30:22 +0100

    fonction pour récupérer et créer une note  affilié au  livre

---
+++ services/NoteServices.ts
@@ -0,0 +1,50 @@
+import { Notes } from "@/models/Notes";
+
+async function getNotesByBook(id: number): Promise<Notes[]>{
+
+    const response = await fetch(`http://localhost:3000/books/${id}/notes`);
+    const data = await response.json();
+    console.log("data notes = ", data);
+
+   const notes = data.map(
+    ({
+      id,
+      bookId,
+      content,
+      dateISO
+    }: {
+      id: Number;
+      bookId: Number;
+      content: String;
+      dateISO: Date;
+    }) => ({
+      id,
+      bookId,
+      content,
+      dateISO
+    })
+);
+    return notes;
+}
+
+
+async function addNote(idBook: number, content: string) {
+    const response = await fetch(`http://localhost:3000/books/${idBook}/notes`, {
+        method: "POST",
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({content})
+    });
+
+    
+    const data = await response.json();
+    
+    console.log("data add = ", data);
+
+    return data;
+}
+
+
+export {
+    addNote, getNotesByBook
+};
+
