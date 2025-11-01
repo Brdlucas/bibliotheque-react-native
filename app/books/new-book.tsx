76e23d26016e4023ed56baad47dd4e0c54749484
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-11-01 17:43:02 +0100

    appel de formcard pour le "fomulaire" de création d'un livre

--- app/books/new-book.tsx
+++ app/books/new-book.tsx
@@ -1,59 +1,70 @@
+import FormCard from "@/components/FormCard";
 import { postNewBook } from "@/services/BookServices";
-import { useRouter } from "expo-router";
+import { useLocalSearchParams, useRouter } from "expo-router";
 import { useState } from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 function newBook() {
-    const [name, onChangeName] = useState("");
-    const [author, onChangeAuthor] = useState("");
-    const [editor, onChangeEditor] = useState("");
-    const [year, onChangeYear] = useState("");
-    const [message, setMessage] = useState("");
+  const { id } = useLocalSearchParams();
 
-    const router = useRouter();
+  const [name, setName] = useState("");
+  const [author, setAuthor] = useState("");
+  const [editor, setEditor] = useState("");
+  const [year, setYear] = useState("");
+  const [message, setMessage] = useState("");
+  const [cover, setCover] = useState("");
 
-    const handleNewBook = async () => {
-        setMessage("");
+  const router = useRouter();
 
-        postNewBook(name, author, editor, Number(year)).then(data => {
-            if(data === 201){
-                 setTimeout(() => {
-              router.push('/')
-            }, 800)
-            setMessage("mise a jour efféctué - redirection")
-        }else {
-            setMessage("erreur lors de la création du livre")
-            }
-        });
-    };
+  const handleNewBook = async () => {
+    setMessage("");
 
-    
+    postNewBook(name, author, editor, Number(year)).then((data) => {
+      if (data && data.status === 201) {
+        console.log(data)
+        setTimeout(() => {
+          router.push(`/books/${data.data.id}`);
+        }, 800);
+        setMessage("mise a jour efféctué - redirection");
+      } else {
+        setMessage("erreur lors de la création du livre");
+      }
+    });
+  };
+
   return (
     <View style={styles.Container}>
-      <Text>nouveaux livre</Text>
-      <TextInput style={styles.Text} value={name} onChangeText={onChangeName} />
-      <TextInput style={styles.Text} value={author} onChangeText={onChangeAuthor}/>
-      <TextInput style={styles.Text} value={editor} onChangeText={onChangeEditor}/>
-      <TextInput style={styles.Text} value={year} onChangeText={onChangeYear}/>
-      <Text>{message && message}</Text>
-      <Button title="créer un nouveau livre"  onPress={() => handleNewBook()}/>
+      <FormCard
+        name={name}
+        author={author}
+        editor={editor}
+        year={year}
+        cover={cover}
+        message={message}
+        setName={setName}
+        setAuthor={setAuthor}
+        setEditor={setEditor}
+        setYear={setYear}
+        setCover={setCover}
+        handleStatusBook={handleNewBook}
+        method={"post"}
+      />
     </View>
-  )
+  );
 }
 
-export default newBook
-
+export default newBook;
 
 const styles = StyleSheet.create({
-    Container: {
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 16,
-        padding: 12
-    },
+  Container: {
+    display: "flex",
+    flexDirection: "column",
+    gap: 16,
+    padding: 12,
+  },
 
-    Text: {
-        padding: 12,
-        backgroundColor: 'lightgray'
-    }
-})
+  Text: {
+    padding: 12,
+    backgroundColor: "lightgray",
+  },
+});
