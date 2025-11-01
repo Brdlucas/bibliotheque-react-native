956b02563a863230089dea2c5a3ce1f3989a6c17
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-11-01 19:28:30 +0100

    ajout de cover pour la création et l'update d'un livre

--- services/BookServices.ts
+++ services/BookServices.ts
@@ -1,27 +1,25 @@
 import { Books } from "@/models/Books";
 
 // penser a changer l'url coté api avec 192.168.0.32 ou localhost
-const urlBack = "localhost"
+const urlBack = "localhost";
 // const urlBack = "192.168.0.32"
 
 async function getBooks(search: string, value: string): Promise<Books[]> {
-
-
   // récupération des books a partir de l'api et suivant le filtre de recherche
   const fetchbooks = async () => {
-      let response;
-      if(!search){
-        response = await fetch(`http://${urlBack}:3000/books`);
-      }else{
-        response = await fetch(`http://${urlBack}:3000/books?${search}=${value}`);
-      }
+    let response;
+    if (!search) {
+      response = await fetch(`http://${urlBack}:3000/books`);
+    } else {
+      response = await fetch(`http://${urlBack}:3000/books?${search}=${value}`);
+    }
     const data = response.json();
     return data;
-  }
+  };
 
   const data = await fetchbooks();
 
-//   mise en forme de la variable books en récupérent les éléments pertinents
+  //   mise en forme de la variable books en récupérent les éléments pertinents
   const books = data.map(
     ({
       id,
@@ -62,9 +60,7 @@ async function getBooks(search: string, value: string): Promise<Books[]> {
   return books;
 }
 
-
-async function getDetailBook(id: number): Promise<Books>{
-
+async function getDetailBook(id: number): Promise<Books> {
   // récupération d'un seul book a partir de l'id
   // const response = await fetch(`https://api.books.tristan-renard.com/books/${id}`);
   const response = await fetch(`http://${urlBack}:3000/books/${id}`);
@@ -73,67 +69,62 @@ async function getDetailBook(id: number): Promise<Books>{
   return data;
 }
 
-async function postNewBook(name: string, author: string, editor: string, year: number){
-
+async function postNewBook(
+  name: string,
+  author: string,
+  editor: string,
+  year: number,
+  cover: string
+) {
   try {
     // création  d'un book a partir des informations fournies
     // const response = await fetch(`https://api.books.tristan-renard.com/books/${id}`);
     const response = await fetch(`http://${urlBack}:3000/books/`, {
       method: "POST",
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify({name, author, editor, year})
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name, author, editor, year, cover }),
     });
     const status = response.status;
     const data = response.ok ? await response.json() : null;
 
     return { status, data };
-
   } catch (error) {
     console.error(error);
   }
-
 }
 
-async function updateBooks(id: number, bookData: Partial<Books>){
-
+async function updateBooks(id: number, bookData: Partial<Books>) {
   try {
     // création  d'un book a partir des informations fournies
     // const response = await fetch(`https://api.books.tristan-renard.com/books/${id}`);
     const response = await fetch(`http://${urlBack}:3000/books/${id}`, {
       method: "PUT",
-      headers: { 'Content-Type': 'application/json'},
-      body: JSON.stringify(bookData)
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(bookData),
     });
 
-
-   const status = response.status;
+    const status = response.status;
     const data = response.ok ? await response.json() : null;
 
     return { status, data };
   } catch (error) {
     console.error(error);
   }
-
 }
 
-
-async function getDeleteBook(id: number){
-
+async function getDeleteBook(id: number) {
   try {
     // récupération d'un seul book a partir de l'id
     const response = await fetch(`http://${urlBack}:3000/books/${id}`, {
-      method: "DELETE"
+      method: "DELETE",
     });
-    
-    if(response.status === 200){
+
+    if (response.status === 200) {
       return response.status;
     }
-    
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-  
-  
 }
 export { getBooks, getDeleteBook, getDetailBook, postNewBook, updateBooks };
 
