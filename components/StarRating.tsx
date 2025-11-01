d3398dd34d523a9a1b91b488dc417fb50da4ee2c
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-11-01 17:45:04 +0100

    changement mineurs

--- components/StarRating.tsx
+++ components/StarRating.tsx
@@ -1,23 +1,25 @@
-
-
-import Ionicons from '@expo/vector-icons/Ionicons';
-import React from 'react';
-import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import React from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 
 interface StarRatingProps {
-    rating: number;
-    onRatingChange: (rating: number) => void;
-    editable: boolean;
-    size: number;
+  rating: number;
+  onRatingChange: (rating: number) => void;
+  editable: boolean;
+  size: number;
 }
 
-export default function StarRating({rating, onRatingChange, editable = true,size = 30 }: StarRatingProps) {
-
-    const handlePress = async (selectRating: number) => {
-        if(editable && onRatingChange){
-            onRatingChange(selectRating);
-        }
+export default function StarRating({
+  rating,
+  onRatingChange,
+  editable = true,
+  size = 30,
+}: StarRatingProps) {
+  const handlePress = async (selectRating: number) => {
+    if (editable && onRatingChange) {
+      onRatingChange(selectRating);
     }
+  };
   return (
     <View style={styles.container}>
       {[1, 2, 3, 4, 5].map((star) => (
@@ -27,24 +29,32 @@ export default function StarRating({rating, onRatingChange, editable = true,size
           disabled={!editable}
           activeOpacity={0.7}
         >
-          <View style={[styles.star, { width: size, height: size}]}>
+          <View style={[styles.star, { width: size, height: size }]}>
             <Star filled={star <= rating} size={size} />
           </View>
         </TouchableOpacity>
       ))}
     </View>
-  )
+  );
 }
-
 
 function Star({ filled, size }: { filled: boolean; size: number }) {
   return (
-    <View style={[styles.starShape, { 
-      width: size, 
-      height: size,
-        }]}>
+    <View
+      style={[
+        styles.starShape,
+        {
+          width: size,
+          height: size,
+        },
+      ]}
+    >
       <View style={styles.starText}>
-        {filled ?<Ionicons name="star-sharp" size={24} color={"#FFD700"} /> :  <Ionicons name="star-outline" size={24} color="black" />}
+        {filled ? (
+          <Ionicons name="star-sharp" size={24} color={"#ffd900ff"} />
+        ) : (
+          <Ionicons name="star-outline" size={24} color="#ffd900ff" />
+        )}
       </View>
     </View>
   );
@@ -52,23 +62,21 @@ function Star({ filled, size }: { filled: boolean; size: number }) {
 
 const styles = StyleSheet.create({
   container: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'lightgray',
-    borderRadius: 8
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
   },
   star: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   starShape: {
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   starText: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
+    justifyContent: "center",
+    alignItems: "center",
+  },
 });
